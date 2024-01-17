509f83ee9f08a2c979e3e5d3a5a3aae762dad03c
G Côte <meltwin158@gmail.com>
2024-01-17 10:14:37 +0100

    Button style

--- src/button/ButtonPage.tsx
+++ src/button/ButtonPage.tsx
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
+import "./buttonpage.scss"
 
 export function ButtonPage() {
     return (<div className="button-page">
         <h1>Click below to generate the mystery box !</h1>
-        <NavLink to="random">Random</NavLink>
+        <NavLink to="random">Get a Mystery Box !</NavLink>
     </div>)
 }
